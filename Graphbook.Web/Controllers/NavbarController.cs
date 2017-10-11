1cb54fc3f5aac4d4ebba2d80f8bd9624c40dd83c
Rafał Hryniewski <racibor@gmail.com>
2017-10-11 23:16:21 +0200

    -

---
+++ Graphbook.Web/Controllers/NavbarController.cs
@@ -0,0 +1,19 @@
+﻿using Graphbook.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Graphbook.Web.Controllers
+{
+    public class NavbarController : Controller
+    {
+        public ActionResult Index()
+        {
+            //TODO: Navbar vm
+
+            return View(new NavbarVM(userFullName: System.Security.Claims.ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value));
+        }
+    }
+}
